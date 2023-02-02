e4d21ffa0f0cf9776f7859c572d82375f2107563
Voffanich <vladimirmatushev@yandex.ru>
2023-02-02 14:37:27 +0300

    added bot_funcs.py with load_config and get_ohlcv_data_binance functions, updated README

--- bot.py
+++ bot.py
@@ -4,31 +4,19 @@ import pprint
 import pandas as pd
 import requests
 
+import bot_funcs as bf
 import levels
 
-with open('config.json') as config_file:
-    config = json.load(config_file)
+config = bf.load_config()
 
 pair = "ETHUSDT" # Trading pair
 pair = "BTCUSDT" # Trading pair
 # pair = "AKROUSDT"
+
 timeframe = config['general']['trading_timeframe'] # Timeframe 
 limit = config['levels']['candle_depth']  # Limit of candles requested 
 
-
-url = f"https://api.binance.com/api/v3/klines?symbol={pair}&interval={timeframe}&limit={limit}"
-
-response = requests.get(url)
-data = response.json()
-
-ohlcv_data = [] # Only open time, open, high, low, close, volume data from response
-for candle in data:
-    ohlcv = candle[0:6] # Open Time, Open, High, Low, Close, Volume
-    ohlcv_data.append(ohlcv)
-   
-
-df = pd.DataFrame(ohlcv_data, columns=["O_time", "Open", "High", "Low", "Close", "Volume"])
-# print(df)
+df = bf.get_ohlcv_data_binance(pair, timeframe, limit)
 
 levels.find_levels(df, timeframe)
 
---
+++ bot_funcs.py
@@ -0,0 +1,25 @@
+import json
+
+import pandas as pd
+import requests
+
+
+def load_config() -> dict:
+    with open('config.json') as config_file:
+        return json.load(config_file)
+    
+def get_ohlcv_data_binance(pair: str, timeframe: str, limit: int = 100) -> pd.DataFrame:
+        
+    url = f"https://api.binance.com/api/v3/klines?symbol={pair}&interval={timeframe}&limit={limit}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    ohlcv_data = [] # Only open time, open, high, low, close, volume data from response
+    for candle in data:
+        ohlcv = candle[0:6] # Open Time, Open, High, Low, Close, Volume
+        ohlcv_data.append(ohlcv)
+    
+
+    return pd.DataFrame(ohlcv_data, columns=["O_time", "Open", "High", "Low", "Close", "Volume"])
+        
